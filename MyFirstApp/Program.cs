fe75517f78def6e108ebc3552b575401313e6c0c
mohamedalmeldeen <mohamed.almeldeen@hotmail.com>
2024-07-14 19:45:45 +0300

    init

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -10,20 +10,20 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
-            string name;
+            /*string name;
             byte age;
             Console.WriteLine("Enter Your Name Please:");
             name = Console.ReadLine();
             Console.WriteLine("and Enter Your age Please:");
             age = Convert.ToByte(Console.ReadLine()); 
-            Console.WriteLine($"Welcome Mr. {name}, Your age is: {age}");
+            Console.WriteLine($"Welcome Mr. {name}, Your age is: {age}");*/
 
-            /*int number = 5;
+            int number = 5;
             string name = "Almeldeen20";
             name = "Mohamed";
             number = "5+3";
             string name = "Almeldeen20";
-            byte age = 32;*/
+            byte age = 32;
             //Console.WriteLine("My Name is: " + name  + "and My Age is: " + age);
             //Console.Write("My Name is: {0}, and My Age is: {1}  and My Age is: " ,name,age);
 
